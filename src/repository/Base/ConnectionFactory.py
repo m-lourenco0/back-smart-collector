b0cb7226cbf7e0781365adb3348afea367db85b4
Marcelo Lourenco <marcelolourenco@relevolab.com>
2022-05-02 00:57:54 -0300

    changing driver

--- src/repository/Base/ConnectionFactory.py
+++ src/repository/Base/ConnectionFactory.py
@@ -1,14 +1,13 @@
 import urllib
 import settings
 from sqlalchemy import create_engine, MetaData, Table, Column, ForeignKey
-import pyodbc
 
 class ConnectionFactory(): 
 
     @staticmethod
     def getInstance():
         #linux
-        strConnect = "Driver={driver};SERVER=" + settings.SQL_SERVER + ";DATABASE="+ settings.SQL_DATABASE +";UID="+ settings.SQL_USER+";PWD="+ settings.SQL_PASS
+        strConnect = "Driver={ODBC Driver 17 for SQL Server};SERVER=" + settings.SQL_SERVER + ";DATABASE="+ settings.SQL_DATABASE +";UID="+ settings.SQL_USER+";PWD="+ settings.SQL_PASS
         #windows
         # strConnect = "Driver={SQL Server Native Client 11.0};SERVER=" + settings.SQL_SERVER + ";DATABASE="+ settings.SQL_DATABASE +";UID="+ settings.SQL_USER+";PWD="+ settings.SQL_PASS
         params = urllib.parse.quote_plus(strConnect)
